6db4632669856c6ed1bc49964db3a4eaaecd8611
Nasir Uddin <uddin.nasir.mym@gmail.com>
2025-04-09 00:15:20 +0600

    updated admin panel

--- js/admin/a_dashboard.js
+++ js/admin/a_dashboard.js
@@ -5,7 +5,41 @@ if (isLoggedIn != "true") {
 
 document.getElementById("aWelcome").innerText = "Welcome Admin";
 
-document.getElementById("made-req").innerText = 0;
-document.getElementById("pen-req").innerText = 0;
-document.getElementById("app-req").innerText = 0;
-document.getElementById("rej-req").innerText = 0;
+// blood stock
+
+const bStock = {
+  //will be replaced by backend data
+  ap: 1000,
+  an: 2000,
+  bp: 1000,
+  bn: 3000,
+  abp: 1500,
+  abn: 2000,
+  op: 800,
+  on: 500,
+};
+
+document.querySelector("#bStock-Ap").innerHTML = `<b>${bStock.ap}</b>`;
+document.querySelector("#bStock-An").innerHTML = `<b>${bStock.an}</b>`;
+document.querySelector("#bStock-Bp").innerHTML = `<b>${bStock.bp}</b>`;
+document.querySelector("#bStock-Bn").innerHTML = `<b>${bStock.bn}</b>`;
+document.querySelector("#bStock-ABp").innerHTML = `<b>${bStock.abp}</b>`;
+document.querySelector("#bStock-ABn").innerHTML = `<b>${bStock.abn}</b>`;
+document.querySelector("#bStock-Op").innerHTML = `<b>${bStock.op}</b>`;
+document.querySelector("#bStock-On").innerHTML = `<b>${bStock.on}</b>`;
+
+let stockUpBtn = document.querySelector("#BStock-upBtn");
+
+stockUpBtn.addEventListener("click", () => {
+  let bGrp = document.querySelector("#BStock-select").value;
+  let bQuantity = document.querySelector("#BStock-quantity").value;
+
+  if (bQuantity < 0) {
+    alert("Please enter a valid quantity.");
+    return;
+  } else if (bQuantity == "") {
+    alert("Please enter a quantity.");
+  } else {
+    document.querySelector(`#bStock-${bGrp}`).innerHTML = `<b>${bQuantity}</b>`;
+  }
+});
